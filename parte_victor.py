aeab65310a26769de9045c383df3b9a6b470615d
VictorAdrian123 <victora.silva@cest.edu.br>
2025-11-12 20:31:33 -0300

    Criei a lista que vai armazenar todas as tarefas, criei uma função menu para mostrar as opções numeradas, e criei uma função main onde fica o loop principal que é o "while true", que vai controlar todo o fluxo do programa chamando as funções correspondentes de cada tarefa que o usuario usar

--- parte_victor.py
+++ parte_victor.py
@@ -1,1 +1,38 @@
-print("Parte do Victor")
+tarefas = []
+
+def menu():
+    print("\n<====== SISTEMA TO DO ======>\n")
+    print()
+    print("1. Adicionar tarefas")
+    print("2. Listar tarefas")
+    print("3. Marcar tarefas como concluida")
+    print("4. Editar tarefas")
+    print("5. Remover tarefas")
+    print("6. Sair")
+
+def main():
+    while True:
+        menu()
+        opcao = input("\nEscolha uma opção: ").strip()
+
+        if opcao == "1":
+            adicionar_tarefa(tarefas)
+        elif opcao == "2":
+            listar_tarefas(tarefas)
+        elif opcao == "3":
+            marcar_concluida(tarefas)
+        elif opcao == "4":
+            editar_tarefa(tarefas)
+        elif opcao == "5":
+            remover_tarefa(tarefas)
+        elif opcao == "6":
+            print("Saindo do sistema...")
+            break
+        else:
+            print("Opção invalida. Tente novamente.")    
+
+if __name__ == "__main__":
+    main()                
+
+
+
