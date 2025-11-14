9e883c3a1d9f39dd66602d635087a2152cd1f243
andrepachecosantos2007-ux <andrepachecosantos2007@gmai.com>
2025-11-13 22:45:20 -0300

    Fiquei responsavel por codar as partes das funções relacionadas ao gerenciamento das tarefas, como adicionar novas funções e listar todas as tarefas cadastradas.
    Fiz a função adicionar_tarefa(tarefas), que recebe o nome digitado pelo usuario, cria um bloco com o nome da tarefa e o status de pendente, e depois adiciona esse bloco em uma lista principal usando append, e no final o usuario recebe uma mensagem confirmando a adição da tarefa.
    
    Também adicionei a função listar_tarefas(tarefas), que será usada para listar as tarefas e exibir cada uma com seunumero, nome e status.

--- parte_pacheco.py
+++ parte_pacheco.py
@@ -1,1 +1,16 @@
+def adicionar_tarefa(tarefas):
+    nome = input("Digite o nome da tarefa: ")
+    tarefa = {"nome": nome, "status": "pendente"}
+    tarefas.append(tarefa)
+    print("Tarefa adicionada com sucesso!")
 
+def listar_tarefas(tarefas):
+    if len(tarefas) == 0:
+        print("Nenhuma tarefa cadastrada.")
+        return
+
+    print("\nLista de tarefas:")
+    for indice, tarefa in enumerate(tarefas):
+        numero = indice + 1
+        print(f"{numero}. {tarefa['nome']} — {tarefa['status']}")
+    print()
